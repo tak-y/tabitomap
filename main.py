eeb9a658f689d835e6580ec5b1517be912e353cb
tak-y <65528705+tak-y@users.noreply.github.com>
2022-12-07 19:49:49 +0900

    main.py renew

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ def visual():
             print(error)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080, host='0.0.0.0') 
+    app.run(debug=True, port=8000, host='0.0.0.0') 
 
 
 
